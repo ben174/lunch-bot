4f0e5bff071e6525f163efcd7f16e8cb03d3a610
Ben Friedland <ben174@gmail.com>
2015-11-10 11:24:47 -0800

    Postgres

--- lunchbot/settings.py
+++ lunchbot/settings.py
@@ -74,11 +74,13 @@ WSGI_APPLICATION = 'lunchbot.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lunchbot',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
