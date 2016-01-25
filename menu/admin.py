db4ceddb53f28367354c7f063f36192bf71ee131
Ben Friedland <ben174@gmail.com>
2016-01-25 09:41:17 -0800

    Add allergen to admin

--- menu/admin.py
+++ menu/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from menu.models import Menu, Meal, MenuItem
+from menu.models import Menu, Meal, MenuItem, Allergen
 
-admin.site.register([Menu, Meal, MenuItem])
+admin.site.register([Menu, Meal, MenuItem, Allergen])
